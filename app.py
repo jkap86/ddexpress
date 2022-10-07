e2f314abd9d9c27cc1769541cb5b085c2ff7d746
jkap86 <jkap86@gmail.com>
2022-10-07 16:33:17 -0400

    -

--- app.py
+++ app.py
@@ -156,7 +156,6 @@ def getPlayerShares():
 def index():
     return app.send_static_file('index.html')
 
-
-@app.route('/<path:path>')
-def static_file(path):
-    return app.send_static_file(path)
+@app.route('/<path>')
+def catch_all(path):
+    return app.send_static_file('index.html')
